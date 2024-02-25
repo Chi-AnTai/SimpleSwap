146342c0850dcb37d6dfd03b4c6319f4a4ef8fcf
Chi-AnTai <andy.chian.tai@gmail.com>
2024-02-25 20:26:47 +0800

    feat: init

---
+++ src/SimpleSwap.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract SimpleSwap {
+  address public token0;
+  address public token1;
+
+  constructor(address _token0, address _token1) {
+    token0 = _token0;
+    token1 = _token1;
+  }
+
+  function addLiquidity1(uint256 amount) public {
+
+  }
+
+  function removeLiquidity1() public {
+
+  }
+
+  function swap1(address _tokenIn, uint256 _amountIn) public {
+
+  }
+}
---
+++ test/SimpleSwap.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract SimpleSwapTest {
+  address public user;
+  address public lp1;
+  address public lp2;
+  TestERC20 public tokenA;
+  TestERC20 public tokenB;
+  SimpleSwap public simpleSwap;
+}
